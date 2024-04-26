85e832e0d3f83a5448e154902df7a3608aead5b3
Sidhartha Paidi <paidisaisidhartha9@gmail.com>
2024-04-26 23:01:03 +0530

    Episode 1

--- Inception_Ep-1/CreateHelloWorld/app.js
+++ Inception_Ep-1/CreateHelloWorld/app.js
@@ -1,5 +1,15 @@
 const heading = React.createElement("h1", {id : "heading"}, "Hello from Sidhu by REACT")
 
+const parent = React.createElement("div", { id: "parent" },
+    [React.createElement("div", { id: "child" }, 
+    [React.createElement("h1", {}, "Hii from nest"), React.createElement("h1", {}, "Hii from nest")]),
+    React.createElement("div", { id: "child2" }, 
+    [React.createElement("h1", {}, "Hii from nest"), React.createElement("h1", {}, "Hii from nest")])]
+)
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
 root.render(heading)
+root.render(parent)
+
+console.log(parent)
